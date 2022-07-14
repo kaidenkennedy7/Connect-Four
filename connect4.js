3a8183decc9dfe00cc3d9c62b2961557e384fe35
kaidenkennedy7 <103951147+kaidenkennedy7@users.noreply.github.com>
2022-07-14 17:45:45 -0400

    fixes

--- connect4.js
+++ connect4.js
@@ -4,12 +4,12 @@
  * column until a player gets four-in-a-row (horiz, vert, or diag) or until
  * board fills (tie)
  */
+
 const WIDTH = 7;
 const HEIGHT = 6;
 
 let currPlayer = 1; // active player: 1 or 2
 let board = []; // array of rows, each row is array of cells  (board[y][x])
-
 /** makeBoard: create in-JS board structure:
  *   board = array of rows, each row is array of cells  (board[y][x])
  */
